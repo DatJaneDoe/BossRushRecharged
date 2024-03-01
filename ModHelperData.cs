a390d6db8dc0958d06f4846278af6c7a6a9f9e09
DatJaneDoe <54993349+DatJaneDoe@users.noreply.github.com>
2024-03-01 14:09:46 -0500

    Added Modhelper Data

---
+++ ModHelperData.cs
@@ -0,0 +1,13 @@
+namespace BossRushRecharged;
+
+public static class ModHelperData
+{
+    public const string WorksOnVersion = "41.0";
+    public const string Version = "1.1.0";
+    public const string Name = "Boss Rush Recharged";
+
+    public const string Description = "Blastpopoulos is back, and hes hotter (and hungrier) than ever! Can your defenses stop his army of boss bloons and desire for grilled cheese?";
+
+    public const string RepoOwner = "DatJaneDoe";
+	
+}
